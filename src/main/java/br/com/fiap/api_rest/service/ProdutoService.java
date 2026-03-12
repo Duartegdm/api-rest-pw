602357059b7114be6e804b33ae42a253791f2f12
duartegdm <gduarte.study@gmail.com>
2026-03-11 23:07:35 -0300

    mudando autowired pra constructorwired e adicionando dto response

--- src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
+++ src/main/java/br/com/fiap/api_rest/service/ProdutoService.java
@@ -1,6 +1,8 @@
 package br.com.fiap.api_rest.service;
 
 import br.com.fiap.api_rest.dto.ProdutoRequest;
+import br.com.fiap.api_rest.dto.ProdutoResponse;
+import br.com.fiap.api_rest.mapper.ProdutoMapper;
 import br.com.fiap.api_rest.model.Produto;
 import br.com.fiap.api_rest.repository.ProdutoRepository;
 import org.springframework.beans.BeanUtils;
@@ -10,12 +12,17 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class ProdutoService {
-    @Autowired
-    private ProdutoRepository produtoRepository;
+    private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper mapper;
 
+    public ProdutoService(ProdutoRepository produtoRepository, ProdutoMapper produtoMapper) {
+        this.produtoRepository = produtoRepository;
+        this.mapper = produtoMapper;
+    }
     // CRUD
     public Produto create(ProdutoRequest produtoRequest) {
         Produto produto = new Produto();
@@ -23,13 +30,20 @@ public class ProdutoService {
         return produtoRepository.save(produto);
     }
 
-    public Produto read(UUID id) {
+    public ProdutoResponse read(UUID id) {
         Optional<Produto> produto =  produtoRepository.findById(id);
-        return produto.orElse(null);
+        if (produto.isEmpty()){
+            return null;
+        }
+        return mapper.produtoToResponse(produto.get());
     }
 
-    public List<Produto> read() {
-        return produtoRepository.findAll();
+    public List<ProdutoResponse> read() {
+        List<Produto> produtos = produtoRepository.findAll();
+        return produtos
+                .stream()
+                .map(mapper::produtoToResponse)
+                .collect(Collectors.toList());
     }
 
     public Produto update(Produto produto) {
