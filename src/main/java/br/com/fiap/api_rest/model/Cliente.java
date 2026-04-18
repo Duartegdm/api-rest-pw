173016dc85e2cf55488133bee925c71dbc6c198d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-17 22:42:02 -0300

    criando os relacionamentos

--- src/main/java/br/com/fiap/api_rest/model/Cliente.java
+++ src/main/java/br/com/fiap/api_rest/model/Cliente.java
@@ -1,9 +1,9 @@
 package br.com.fiap.api_rest.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Cliente {
@@ -17,4 +17,57 @@ public class Cliente {
     private LocalDate dataNascimento;
 
     private String Telefone;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_endereco")
+    private Endereco endereco;
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        Telefone = telefone;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
--- src/main/java/br/com/fiap/api_rest/model/Pedido.java
+++ src/main/java/br/com/fiap/api_rest/model/Pedido.java
@@ -1,10 +1,9 @@
 package br.com.fiap.api_rest.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,10 +12,64 @@ import java.util.UUID;
 public class Pedido {
     @Id
     private UUID id;
-
-    private LocalDate data;
-
+    private StatusPedido status;
+    private LocalDateTime data;
+    private double valor;
+    @ManyToOne
+    @JoinColumn(name = "id_cliente")
+    private Cliente cliente;
+    @ManyToMany
+    @JoinTable(name = "produto_pedido",
+            joinColumns = @JoinColumn(name = "id_produto", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_pedido", referencedColumnName = "id"))
     private List<Produto> produtos;
 
-    private float valor;
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
 }
